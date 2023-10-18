ecf52448ecb95b94721fbe900b7d332d4a88c1be
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2023-10-18 17:47:45 +0530

    Lab on JS Storage

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 //Challenge 4: Using clear() method to remove all the data stored in your browser's localStorage and print the localStorage in the console.
 
 //Challenge 5: Using JSON.stringify() convert the given object into the string and print the same.
-const user = {
+var user = {
   firstName: 'Rajat',
   age: 25,
   state: 'Punjab',
@@ -19,10 +19,10 @@ localStorage.setItem("age", user.age);
 localStorage.setItem("state", user.state);
 localStorage.setItem("skills", JSON.stringify(user.skills));
 
-const storedFirstName = localStorage.getItem("firstName");
-const storedAge = localStorage.getItem("age");
-const storedstate = localStorage.getItem("state")
-const storedSkills = JSON.parse(localStorage.getItem("skills"));
+var storedFirstName = localStorage.getItem("firstName");
+var storedAge = localStorage.getItem("age");
+var storedstate = localStorage.getItem("state")
+var storedSkills = JSON.parse(localStorage.getItem("skills"));
 console.log("First Name:", storedFirstName);
 console.log("Age:", storedAge);
 console.log("state:", storedstate);
@@ -32,5 +32,5 @@ localStorage.removeItem("state");
 
 localStorage.clear();
 
-const userString = JSON.stringify(user);
+var userString = JSON.stringify(user);
 console.log(userString);
